ff03f985be83ef0989465c4ce6da51a27fd405d5
Dominic Fox <poetix@users.noreply.github.com>
2017-03-07 14:00:36 +0000

    Initial commit from the atomist bot

---
+++ src/main/java/com/opencredo/sandbox/rest/RestSandboxApplication.java
@@ -0,0 +1,12 @@
+package com.opencredo.sandbox.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSandboxApplication.class, args);
+    }
+}
